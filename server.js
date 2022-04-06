e618087976e1f9d7b0bd9b951e5d093bd983b849
Andrew McInally <realfunnyestate@yahoo.com>
2022-04-06 12:01:35 -0600

    almost working

--- server.js
+++ server.js
@@ -8,9 +8,57 @@ const app = express();
 
 app.use(express.static("public"));
 
-app .use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+function writeNewNote(body, notesArray) {
+  const note = body.note;
+  notesArray.push(note);
+
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+}
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeNewNote(file, parsedData);
+    }
+  });
+};
+
+//ROUTES
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/")));
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+app.post("/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully`);
+  } else {
+    res.error("Error adding note");
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`Love you Dad, on port ${PORT}`)
+  console.log(`Love you Dad, on port ${PORT}`);
 });
